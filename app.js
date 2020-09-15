0f3d22e7f8dec79b52aafc5df23d4711956a9975
Kamran Sher Afgan <64993556+Kamran-SherAfgan@users.noreply.github.com>
2020-09-15 16:13:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function facebook_login() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+      }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        // ...
+      });
+}
+
 function send(){
     var value = document.getElementById('value');
     var p = document.createElement('p');
